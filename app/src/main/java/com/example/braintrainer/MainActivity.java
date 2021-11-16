a255f3f9f8c2170716b258ce205b6f09b96ffb4f
Junaid2478 <79209011+Junaid2478@users.noreply.github.com>
2021-11-16 05:27:57 +0000

    Added background music, Fixed division symbol being added and the point counter not cutting off after 10.

--- app/src/main/java/com/example/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -2,19 +2,12 @@ package com.example.braintrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Random;
 
